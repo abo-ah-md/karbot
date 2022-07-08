f5daedcdceba9e71fd66a00541552595c38a2a35
Abdullah-sh <abo-ah-md@users.noreply.github.com>
2022-07-09 02:10:41 +0300

    first-kabot-test

---
+++ main.js
@@ -0,0 +1,39 @@
+
+const list_cards= document.querySelectorAll('.main-container-list');
+const main_container= document.querySelector('.main-1-container');
+const opnion=document.querySelectorAll('#main-container-opnion');
+
+
+//moving animation event
+main_container.addEventListener("mousemove", (e)=> {
+let xAxis = (window.innerWidth/ 2-e.pageX)/20;
+let yAxis = (window.innerHeight/2-e.pageY)/20;
+list_cards[0].style.transform=`rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+list_cards[1].style.transform=`rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+list_cards[2].style.transform=`rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+list_cards[3].style.transform=`rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+
+});
+
+main_container.addEventListener('moseenter',(e)=>
+{
+    list_cards[1].style.transition='none';
+    list_cards[0].style.transition='none';
+    list_cards[2].style.transition='none';
+    list_cards[3].style.transition='none';
+});
+
+main_container.addEventListener("mouseleave",(e)=>
+{
+    list_cards[1].style.transition='all 0.5 ease';
+    list_cards[0].style.transition='all 0.5 ease';
+    list_cards[2].style.transition='all 0.5 ease';
+    list_cards[3].style.transition='all 0.5 ease';
+
+    list_cards[0].style.transform=`rotateY(0deg) rotateX(0deg)`;
+    list_cards[1].style.transform=`rotateY(0deg) rotateX(0deg)`;
+    list_cards[2].style.transform=`rotateY(0deg) rotateX(0deg)`;
+    list_cards[3].style.transform=`rotateY(0deg) rotateX(0deg)`;
+}
+
+);
